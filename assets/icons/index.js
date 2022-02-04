f92d16be684dcecf63e7cfa3e99e537fadb3ce43
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-02-04 23:51:47 +0800

    Add icon used for payment options

---
+++ assets/icons/index.js
@@ -0,0 +1,7 @@
+import userIcon from './user.png';
+import cashoutIcon from './cashout.png';
+import payIcon from './pay.png';
+import scanIcon from './scan.png';
+import transferIcon from './transfer.png';
+
+export {userIcon, cashoutIcon, payIcon, scanIcon, transferIcon};
