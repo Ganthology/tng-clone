b5441d75264aba8d8b90270b5373bdc94a6ac415
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-02-05 23:02:58 +0800

    Add image needed for highlights section

---
+++ assets/ads/index.js
@@ -0,0 +1,6 @@
+import adImage1 from './adImage1.jpg';
+import adImage2 from './adImage2.png';
+import adImage3 from './adImage3.jpg';
+import adImage4 from './adImage4.jpg';
+
+export {adImage1, adImage2, adImage3, adImage4};
