19fa8e1da4c984e7a842c2edf66b81cd9734dd72
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-02-06 22:32:59 +0800

    Add promotion images

--- assets/ads/index.js
+++ assets/ads/index.js
@@ -2,5 +2,8 @@ import adImage1 from './adImage1.jpg';
 import adImage2 from './adImage2.png';
 import adImage3 from './adImage3.jpg';
 import adImage4 from './adImage4.jpg';
+import adImage5 from './adImage5.jpg';
+import adImage6 from './adImage6.png';
+import adImage7 from './adImage7.png';
 
-export {adImage1, adImage2, adImage3, adImage4};
+export {adImage1, adImage2, adImage3, adImage4, adImage5, adImage6, adImage7};
