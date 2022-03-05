47dfa12c33f9ae3424676d0843dd947a90ea131b
Ray Gan Boon Kit <gbk1386@gmail.com>
2022-03-06 01:58:18 +0800

    Add brief outline for Account Screen

--- screens/AccountScreen.js
+++ screens/AccountScreen.js
@@ -7,16 +7,62 @@ import {
   Text,
   StatusBar,
   Dimensions,
+  FlatList,
 } from 'react-native';
 import {MyStatusBar, Navbar} from '../components';
 
 const AccountScreen = () => {
+  const {width, height} = Dimensions.get('window');
+
   return (
     <View>
       {StatusBar.setBarStyle('light-content', true)}
       <MyStatusBar backgroundColor='#1976d2' barStyle='light-content' />
       <Navbar backgroundColor={'#1976d2'} />
-      <Text>This is AccountScreen</Text>
+      <View
+        id={'Upper Container'}
+        style={{
+          justifyContent: 'space-evenly',
+          height: height * 0.35,
+          backgroundColor: '#1976d2',
+        }}>
+        <View
+          id={'Account Header Container'}
+          style={{flexDirection: 'row', justifyContent: 'space-evenly'}}>
+          <View id={'Inbox Container'}>
+            <Text>Email Icon</Text>
+            <Text>Inbox</Text>
+          </View>
+          <View>
+            <Text>Account Icon</Text>
+          </View>
+          <View>
+            <Text>Edit Info Icon</Text>
+            <Text>Edit Info</Text>
+          </View>
+        </View>
+        <View id={'Account Details Container'} style={{alignItems: 'center'}}>
+          <Text>Gan Boon Kit</Text>
+          <View style={{flexDirection: 'row'}}>
+            <Text>DuitNow Icon</Text>
+            <Text>110629305423</Text>
+            <Text>Copy Icon</Text>
+          </View>
+        </View>
+        <View
+          id={'Upgrade Premium Container'}
+          style={{
+            flexDirection: 'row',
+            alignSelf: 'center',
+            width: width * 0.9,
+          }}>
+          <Text>Lock Icon</Text>
+          <View style={{width: width * 0.75}}>
+            <Text>Upgrade to the PREMIUM tier to unlock more benefits!</Text>
+            <Text>Upgrade Now</Text>
+          </View>
+        </View>
+      </View>
     </View>
   );
 };
